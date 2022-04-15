39cd4cb0c583d62f154224d81718409ac1ba104d
Thiago Soares <thiago.soaresm19@gmail.com>
2022-04-15 09:39:29 -0300

    revert merge with toogle sidebar

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,9 +1,7 @@
 import mySelectComponent from "./global/mySelect";
 import dynamicFormComponent from "./global/dynamicForm";
-import sidebarComponent from "./global/sidebar";
 
 export default [
   {'name': 'vue-my-select', component: mySelectComponent},
-  {'name': 'vue-dynamic-form', component: dynamicFormComponent}
-  {'name': 'vue-sidebar', component: sidebarComponent}
+  {'name': 'vue-dynamic-form', component: dynamicFormComponent},
 ]
